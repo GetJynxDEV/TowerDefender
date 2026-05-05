b832456e00a1544434cc77a046ccb8c7de5e0fd8
SantiThesis <marc.santiago@my.jru.edu>
2026-05-05 08:09:59 +0800

    Created Final Game Scene
    Combine Tower test and enemy wave test.

---
+++ Assets/Scripts/Core/LevelManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
