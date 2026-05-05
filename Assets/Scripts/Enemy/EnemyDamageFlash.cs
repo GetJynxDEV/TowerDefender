5e7adbe75d5d7b5113a182553fb5c9356ced73a7
Shann Stephen Enciso <Shann.enciso@gmail.com>
2026-05-05 09:39:16 +0800

    Enemy Damage Flash

---
+++ Assets/Scripts/Enemy/EnemyDamageFlash.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyDamageFlash : MonoBehaviour //NEW SCRIPT
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private float _flashDuration = 0.1f;
+    [SerializeField] private Color _flashColor = Color.red;
+
+    private Color _originalColor;
+    private Coroutine _flashCoroutine;
+    private Health _health;
+
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+        _originalColor = _spriteRenderer.color;
+    }
+
+    private void OnEnable()
+    {
+        _health.onHit += TriggerFlash;
+    }
+
+    private void OnDisable()
+    {
+        _health.onHit -= TriggerFlash;
+    }
+
+    private void TriggerFlash()
+    {
+        if (_flashCoroutine != null) StopCoroutine(_flashCoroutine);
+        _flashCoroutine = StartCoroutine(FlashCoroutine());
+    }
+
+    private IEnumerator FlashCoroutine()
+    {
+        _spriteRenderer.color = _flashColor;
+        yield return new WaitForSeconds(_flashDuration);
+        _spriteRenderer.color = _originalColor;
+        _flashCoroutine = null;
+    }
+}
